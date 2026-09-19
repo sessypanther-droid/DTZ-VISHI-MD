4fafe2a66f4ef400c7c3b5ed94b068a787f60763
DORA-MD <savithasudda@gmail.com>
2026-09-19 18:36:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ fs.mkdirSync(AUTH_DIR, { recursive: true });
 
 const activeSockets = new Map();
 
-const MONGODB_URI = process.env.MONGODB_URI;
+mongodb+srv://<db_username>:uEwJp0ACjFtHvZGk@cluster0.n4asy3o.mongodb.net/ ;
 
 let mongoClient = null;
 let mongoState = MONGODB_URI ? "connecting" : "not-configured";
